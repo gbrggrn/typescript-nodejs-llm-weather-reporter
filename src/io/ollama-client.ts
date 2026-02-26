7bf4b676c99d2c4f854b97bb939fc220ac50d779
gbrggrn <gustberg107@student.kau.se>
2026-02-26 21:22:30 +0100

    Move to functional architechture + prompt draft

--- src/io/ollama-client.ts
+++ src/io/ollama-client.ts
@@ -17,3 +17,19 @@ async function pingAI() {
         console.log("[ollama-client] Connection failed: ", error)
     }
 }
+
+export async function generateScheduleData() {
+    console.log("[ollama-client] Generating schedule data...")
+
+    try {
+        const response = await remoteOllama.generate({
+            model: 'llama3.1',
+            prompt: 'A prompt will be added here. For now just respond with a limerick about horses.',
+            stream: false
+        });
+
+        console.log("[ollama-client] Generated schedule data: ", response.response)
+    } catch (error) {
+        console.log("[ollama-client] Could not generate schedule data: ", error)
+    }
+}
